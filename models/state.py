bde90c0270b452c7d422d910b7a92fc83feb3eb9
Louis Toro <louistororosario@gmail.com>
2024-03-18 07:03:01 -0400

    create a relationship with city

--- models/state.py
+++ models/state.py
@@ -3,10 +3,27 @@
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+import models
+import shlex
 
 
 class State(BaseModel):
     """ State class """
-    __tablename__ = 'cities'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="State", cascade="all, delete")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
