c09743a6b9aa25d447f9bd3b7c9b411799c8fc77
Louis Toro <louistororosario@gmail.com>
2024-03-18 07:01:50 -0400

    put two colums to cities table

--- models/city.py
+++ models/city.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationaship
-from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+
     __tablename__ = 'cities'
+
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states_id'), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
