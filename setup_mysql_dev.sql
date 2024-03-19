735969bbd48f3b1f317feca373afe8619cd09595
Livanhernandez <livanhernandez9@gmail.com>
2024-03-18 22:31:49 -0400

    fixed file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,3 +4,12 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
+
+-- setup_mysql_dev.sql
+CREATE TABLE IF NOT EXISTS states (
+    id VARCHAR(60) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
