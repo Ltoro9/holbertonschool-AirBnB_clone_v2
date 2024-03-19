b2ae9dd1619227d15ae3caec3fb1f73c8047ff3d
Livanhernandez <livanhernandez9@gmail.com>
2024-03-19 00:17:47 -0400

    fixed files

--- create_table.sql
+++ create_table.sql
@@ -1,7 +1,18 @@
 -- script that creates tables in database
-CREATE TABLE IF NOT EXISTS states (
-    id VARCHAR(60) NOT NULL,
-    name VARCHAR(128) NOT NULL,
-    created_at DATETIME NOT NULL,
-    PRIMARY KEY (id)
-):
+CREATE TABLE states (
+  id VARCHAR(60) NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  name VARCHAR(128) NOT NULL,
+  PRIMARY KEY(id)
+  );
+
+CREATE TABLE cities (
+  id VARCHAR(60) NOT NULL,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  state_id VARCHAR(60) NOT NULL,
+  PRIMARY KEY (id),
+  KEY state_id (state_id),
+  CONSTRAINT fk_state_id FOREIGN KEY (state_id) REFERENCES states (id)
+  );
