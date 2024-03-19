35d84677a4c97434768f84cc7afc3b443ca16ba2
Livanhernandez <livanhernandez9@gmail.com>
2024-03-19 00:19:09 -0400

    updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,8 @@
--- script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- Write a script that prepares a MySQL server for the project:
 
--- setup_mysql_dev.sql
-CREATE TABLE IF NOT EXISTS states (
-    id VARCHAR(60) NOT NULL,
-    name VARCHAR(128) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
