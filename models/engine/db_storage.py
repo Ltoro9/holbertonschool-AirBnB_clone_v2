5387938f2d4c740a2453afdc123feca55a6afa16
Louis Toro <louistororosario@gmail.com>
2024-03-20 14:44:23 -0400

    add some comments

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """
-
+Module for database storage
 """
-
 
 
 from sqlalchemy import create_engine
 from os import getenv
 import sys
-from models.base_model import Base, BaseModel
+from models.base_model import Base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models.user import User
+
 
 
 class DBStorage:
@@ -24,54 +21,72 @@ class DBStorage:
     __session = None
 
     def __init__(self):
+        """
+        Initialize DBStorage class
+        """
         user = getenv("HBNB_MYSQL_USER")
         passwd = getenv("HBNB_MYSQL_PWD")
         host = getenv("HBNB_MYSQL_HOST")
         database = getenv("HBNB_MYSQL_DB")
         env = getenv("HBNB_ENV")
-        # print(user, passwd, host, database, env)
 
         self.__engine = create_engine(
             f"mysql+mysqldb://{user}:{passwd}@{host}/{database}",
             pool_pre_ping=True)
-
-        if getenv("HBNB_ENV") == "test":
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        dic = {}
+        """
+        Query all objects
+        """
+        obj_dict = {}
         if cls:
             if isinstance(cls, str):
                 cls = getattr(sys.modules[__name__], cls)
             query = self.__session.query(cls)
-            for elem in query:
-                key = "{}.{}".format(type(elem).__name__, elem.id)
-                dic[key] = elem
+            for obj in query:
+                key = f"{type(obj).__name__}.{obj.id}"
+                obj_dict[key] = obj
         else:
-            classes = [State, City]
+            classes = [User, State, City]
             for cls in classes:
                 query = self.__session.query(cls)
-                for elem in query:
-                    key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dic[key] = elem
-        return dic
+                for obj in query:
+                    key = f"{type(obj).__name__}.{obj.id}"
+                    obj_dict[key] = obj
+        return obj_dict
 
     def new(self, obj):
+        """
+        Add object to current database session
+        """
         self.__session.add(obj)
 
     def save(self):
+        """
+        Commit all changes of the current database session
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
+        """
+        Delete from the current database session
+        """
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
+        """
+        Create all tables in the database
+        """
         Base.metadata.create_all(self.__engine)
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                             expire_on_commit=False))
         self.__session = Session()
 
     def close(self):
+        """
+        Close current session
+        """
         self.__session.close()
-
