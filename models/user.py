ec39fcebd26e05f98a8cf350e53213cddc68e745
Livanhernandez <livanhernandez9@gmail.com>
2024-03-19 22:07:01 -0400

    updated file

--- models/user.py
+++ models/user.py
@@ -2,14 +2,16 @@
 """comment"""
 
 
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     '''
         User class
     '''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
