fac7bf80cd6142555ce7726a1df7af5906290728
Livanhernandez <livanhernandez9@gmail.com>
2024-03-20 21:28:51 -0400

    fixed amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table, ForeignKey
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-
-class Amenity(BaseModel, Base):
-    """Amenity class to store amenity information"""
+class Amenity(BaseModel):
+    """ Amenity class to store amenity information """
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
 
-    places = relationship("Place", secondary="place_amenity", back_populates="amenities")
-
-    place_amenity = Table('place_amenity', BaseModel.metadata,
-                          Column('place_id', String(60), ForeignKey('place.id'), primary_key=True),
-                          Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True)
-                          )
+    # Define Many-To-Many relationship with Place
+    place_amenities = relationship("Place", secondary="place_amenity", back_populates="amenities")
