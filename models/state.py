a6e02a9b1d03c3941f71cd177ce25adbe307addb
Livanhernandez <livanhernandez9@gmail.com>
2024-03-21 00:01:53 -0400

    fixed pycode

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     cities = relationship("City", backref="state",
-                        cascade="all, delete-orphan")
+                          cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
