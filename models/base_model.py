01eee3a6995f796e48e27166fa1abba9a7c7fd4e
Louis Toro <louistororosario@gmail.com>
2024-03-21 00:08:09 -0400

    change init

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,8 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
+    __tablename__ = None
+
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
