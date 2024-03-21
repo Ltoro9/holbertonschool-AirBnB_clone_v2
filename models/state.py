d27f11fab775416a25f6d964542accb604e20311
Louis Toro <louistororosario@gmail.com>
2024-03-20 23:58:22 -0400

    testing self.save() for infinite loop

--- models/state.py
+++ models/state.py
@@ -13,13 +13,11 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+                        cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.save()
-    
-        self.save()
 
     @property
     def cities(self):
