3ad7c65f218952989dc4e763ed4354070a0462f5
Ltoro9 <louistororosario@gmail.com>
2024-04-12 16:23:14 -0400

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,28 +1,56 @@
+#!/usr/bin/python3
+"""
+    script that starts a Flask web application
+"""
+
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
-    return(f"Hello HBNB!")
+    """
+        print Hello HBNB in flask web app
+    """
+    return (f"Hello HBNB!")
+
 
 @app.route("/hbnb", strict_slashes=False)
 def no_hello():
-    return(f"HBNB")
+    """
+        print HBNB in flask web app
+    """
+    return (f"HBNB")
 
-@app.route("/c/", strict_slashes=False)
+
 @app.route("/c/<text>", strict_slashes=False)
-def C_is_fun(text="is_cool"):
+def C_is_fun(text):
+    """
+        display “C ” followed by the value of the text variable
+    """
     return f'C {text.replace("_", " ")}'
+
 
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_not_fun(text="is_cool"):
+    """
+        display “Python ”, followed by the value of the text variable
+        replace underscore _ symbols with a space
+    """
     return f'Python {text.replace("_", " ")}'
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def i_love_numbers(n):
+    """
+        display “n is a number” only if n is an integer
+    """
     return f'{n} is a number'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
