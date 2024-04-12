b2ce2e30b2b7b02f0fcb5de28517f14033964426
Ltoro9 <louistororosario@gmail.com>
2024-04-12 15:39:10 -0400

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,20 @@
+#!/usr/bin/python3
+"""
+    script that starts a Flask web application
+"""
+
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
+    """
+        print hello in flask web app
+    """
     return(f"Hello HBNB!")
 
 if __name__ == "__main__":
